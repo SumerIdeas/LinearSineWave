e9e29f229a90b542b7017e19bc8c4291aea194da
dcrump <donniecrump@protonmail.com>
2024-12-06 21:40:35 -0500

    Project Reorganization

--- LinearSineWave/ViewModels/MainViewModel.cs
+++ LinearSineWave/ViewModels/MainViewModel.cs
@@ -1,8 +1,25 @@
-﻿namespace LinearSineWave.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace LinearSineWave.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    private readonly ArtistTrackViewModel _artistTrackPage = new();
+    
+    public string? CurrentTrackName { get; set; }
+    public string? CurrentTrackAlbum { get; set; }
+    public string? CurrentTrackArtist { get; set; }
+    public float TrackPosition { get; set; } = -1.0F;
+    public float TrackLength { get; set; } = 0.0F;
+    
+    public string TrackPositionLabel
+    {
+        get => TrackPosition == -1 ? "00.00" : TrackPosition.ToString("00.00");
+    }
+    public string TrackLengthLabel
+    {
+        get => TrackLength.ToString("00.00");
+    }
+    
+    public ArtistTrackViewModel ArtistTrackPage => _artistTrackPage;
 }
